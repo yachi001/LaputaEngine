10ba68f3a19d4b01f987cb91d3a035e79ab97f2b
ya <yachil@sfu.ca>
2014-03-18 04:14:47 -0700

    add log4cplus

---
+++ LaputaEngine/LaputaLoggingDemo/LaputaLoggingDemo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "..\LaputaLogging\LaputaLogging.h"
+
+
+int main()
+{
+    testLogToConsole();
+    testLogToFile(); // save under the folder: LaputaEngine\LaputaLoggingDemo\log
+    return 0;
+}
